2a10242af8886a676f4eab0b5d89ec6d596aaefd
Klemmbaustein <83748124+Klemmbaustein@users.noreply.github.com>
2023-07-13 18:30:49 +0200

    Update update.bat

--- NorthstarInstaller/update.bat
+++ NorthstarInstaller/update.bat
@@ -1,5 +1,8 @@
 @echo off
+:: Give some time for the installer to exit after it has called the update script.
 ping 127.0.0.1 -n 2  nul
+:: Copy the newly installed installer version into the current directory.
 xcopy /s/e/y Data\temp\install .\
+:: Start the installer again
 start NorthstarInstaller.exe
 exit
