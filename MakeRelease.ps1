fbd650add8067c611a53170fdf316428a924dc5c
Klemmbaustein <83748124+Klemmbaustein@users.noreply.github.com>
2023-10-20 22:02:55 +0200

    license update

--- MakeRelease.ps1
+++ MakeRelease.ps1
@@ -3,3 +3,4 @@ cp NorthstarInstaller/Data Release/Data -Recurse -force
 cp NorthstarInstaller/Shaders Release/Shaders -Recurse -force
 cp NorthstarInstaller/update.bat Release/update.bat -Recurse -force
 cp KlemmUI\SDL\VisualC\SDL\x64\Release\SDL2.dll Release\SDL2.dll
+cp License.txt Release\License.txt
