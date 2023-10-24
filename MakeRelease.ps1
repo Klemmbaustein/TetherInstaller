e454c938869793d6bd0357bc11b779b6c8cafc9e
Klemmbaustein <83748124+Klemmbaustein@users.noreply.github.com>
2023-10-24 20:58:34 +0200

    fixed shaders being included twice

--- MakeRelease.ps1
+++ MakeRelease.ps1
@@ -1,6 +1,5 @@
 mkdir Release -force
 cp NorthstarInstaller/Data Release/Data -Recurse -force
-cp NorthstarInstaller/Shaders Release/Shaders -Recurse -force
 cp NorthstarInstaller/update.bat Release/update.bat -Recurse -force
 cp KlemmUI\SDL\VisualC\SDL\x64\Release\SDL2.dll Release\SDL2.dll
 cp License.txt Release\License.txt
