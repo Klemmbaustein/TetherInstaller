6bff853c3b4bafa1c1ce0201d275649495f8d858
Klemmbaustein <83748124+Klemmbaustein@users.noreply.github.com>
2024-03-02 16:53:03 +0100

    fixed setup for new KlemmUI  version

--- Setup.ps1
+++ Setup.ps1
@@ -1,18 +1,18 @@
 
-if (!(Test-Path -Path KlemmUI/SDL/include))
+if (!(Test-Path -Path Dependencies\KlemmUI/SDL/include))
 {
 	echo "SDL directory seems to be empty or missing. Verify you have cloned the repo with submodules or run 'git submodule update --init --recursive'"
 	exit
 }
 
-if (!(Test-Path -Path SDL\VisualC\SDL\x64\Release))
+if (!(Test-Path -Path Dependencies\SDL\VisualC\SDL\x64\Release))
 {
 	cd KlemmUI/
 	./Setup.ps1
 	cd ..
 }
 
-cp KlemmUI\SDL\VisualC\SDL\x64\Release\SDL2.dll NorthstarInstaller
+cp KlemmUI\Dependencies\SDL\VisualC\SDL\x64\Release\SDL2.dll NorthstarInstaller
 
 cd curl
 cmake -DCURL_USE_SCHANNEL=on -DBUILD_SHARED_LIBS=off -DCMAKE_C_COMPILER=cl -B Build
